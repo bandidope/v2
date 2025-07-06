5b70bbc447feded892fa40463bf3bfe1d0250183
YALLICO OWNER <yallico2024@gmail.com>
2025-07-06 12:07:15 +0200

    Eazzy Actualizado V2 🤍
    Eazzy Actualizado V2 🤍

--- plugins/group-canal.js
+++ plugins/group-canal.js
@@ -9,7 +9,7 @@ let yaNotificados = new Set(
 );
 
 const enviarAvisoCanal = async (conn, notifyChat = null) => {
-  const mensaje = `⭐ *¡Te invitamos a nuestro canal!* 🌟\n\nEste es el canal oficial 📢 de *Barboza*:\n\n👉 https://whatsapp.com/channel/0029Vaua0ZD3gvWjQaIpSy18\n\nSíguelo para estar al tanto de *comandos, novedades y actualizaciones*. ¡Gracias por tu apoyo! 🙌`;
+  const mensaje = `⭐ *¡Te invitamos a nuestro canal!* 🌟\n\nEste es el canal oficial 📢 de *Eazzy*:\n\n👉 https://whatsapp.com/channel/0029Vb5oUp43LdQUVViHwc0m\n\nSíguelo para estar al tanto de *comandos, novedades y actualizaciones*. ¡Gracias por tu apoyo! 🙌`;
 
   const chats = Object.entries(conn.chats).filter(([jid, chat]) => jid && chat.isChats);
   let usuarios = [];
@@ -65,7 +65,7 @@ const handler = async (m, { conn, isOwner}) => {
 handler.help = ['canal'];
 handler.tags = ['owner'];
 handler.command = ['canal'];
-handler.owner = true;
+handler.owner = false;
 
 // También se puede activar al arrancar el bot
 handler.run = async (conn) => {
