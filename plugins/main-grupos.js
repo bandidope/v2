57dc89af11b89c0f0fbc9a9d985acd19a2614130
YALLICO OWNER <yallico2024@gmail.com>
2025-07-06 12:50:48 +0200

    Eazzy Actualizado V2 🤍
    Eazzy Actualizado V2 🤍

--- plugins/main-grupos.js
+++ plugins/main-grupos.js
@@ -2,16 +2,16 @@ import fetch from 'node-fetch';
 
 let handler = async (m, { conn, usedPrefix, command }) => {
     let grupos = "*Hola!, te invito a unirte a los grupos oficiales del Bot para convivir con la comunidad* ⭐\n\n" +
-                 "1-Barboza\n" +
-                 "*✰* https://chat.whatsapp.com/CBuLXuVZcg9FEfCSHiY6b0" +
+                 "1 - Pruebas Eazzy X Bot\n" +
+                 "*✰* https://chat.whatsapp.com/IFkJ3LVePPC0FDGFuDul98" +
                  "*─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*\n\n" +
                  "➠ Enlace anulado? entre aquí! \n\n" +
-                 "⭐ Canal :\n" +
-                 "*✰*https://whatsapp.com/channel/0029Vb8kvXUBfxnzYWsbS81I" +
-                 "> By Barboza";
+                 "⭐ Reclutamiento  Eazzy :\n" +
+                 "*✰*https://chat.whatsapp.com/KyG4Y5cGAlV1LdhB27aZOI" +
+                 "> By Eazzy X";
 
     // Asegúrate de definir 'imagen2' correctamente antes de usarlo
-    let imagen2 = 'https://qu.ax/Mvhfa.jpg';
+    let imagen2 = 'https://qu.ax/cqUYc.jpg';
 
     // Define los emojis que quieres usar
     let emojis = '🍁';
