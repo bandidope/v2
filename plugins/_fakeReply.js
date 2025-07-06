5160acd320db0ff67d41611cbed247eee3eca52b
YALLICO OWNER <yallico2024@gmail.com>
2025-07-06 11:54:57 +0200

    Eazzy Actualizado V2 🤍
    Eazzy Actualizado V2 🤍

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch'
 
 export async function before(m, { conn }) {
-let name = `ᥴһᥲᥒᥒᥱᥣ-sᥲsᥙkᥱ ᑲ᥆𝗍 mძ 🌀`
-let imagenes = ["https://files.catbox.moe/6dewf4.jpg",
-"https://files.catbox.moe/6dewf4.jpg",
-"https://files.catbox.moe/6dewf4.jpg",
-"https://files.catbox.moe/6dewf4.jpg",]
+let name = `Ｃｈａｎｎｅｌ   Ｅａｚｚｙ   Ｘ   Ｂｏｔ 🤍`
+let imagenes = ["https://qu.ax/cqUYc.jpg",
+"https://qu.ax/cqUYc.jpg",
+"https://qu.ax/cqUYc.jpg",
+"https://qu.ax/cqUYc.jpg",]
 
 let icono = imagenes[Math.floor(Math.random() * imagenes.length)]
 
@@ -13,7 +13,7 @@ global.rcanal = {
  contextInfo: {
              isForwarded: true,
     forwardedNewsletterMessageInfo: {
-      newsletterJid: "120363419364337473@newsletter",
+      newsletterJid: "120363419947391620@newsletter",
       serverMessageId: 100,
       newsletterName: name,
    }, 
@@ -33,12 +33,12 @@ global.rcanal = {
 
 
 global.icono = [ 
-'https://qu.ax/yyCo.jpeg',
-'https://qu.ax/yyCo.jpeg',
-'https://qu.ax/qJch.jpeg',
-'https://qu.ax/qJch.jpeg',
-'https://qu.ax/CHRS.jpeg',
-'https://qu.ax/CHRS.jpeg',
+'https://qu.ax/cqUYc.jpg',
+'https://qu.ax/cqUYc.jpg',
+'https://qu.ax/cqUYc.jpg',
+'https://qu.ax/cqUYc.jpg',
+'https://qu.ax/cqUYc.jpg',
+'https://qu.ax/cqUYc.jpg',
 ].getRandom()
 
         global.fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': fs.readFileSync('./storage/img/catalogo.png'), thumbnail: fs.readFileSync('./storage/img/catalogo.png'), sendEphemeral: true } } }
@@ -62,7 +62,7 @@ global.icono = [
     contextInfo: {
             isForwarded: true,
     forwardedNewsletterMessageInfo: {
-      newsletterJid: "120363419364337473@newsletter",
+      newsletterJid: "120363419947391620@newsletter",
       serverMessageId: 100,
       newsletterName: name,
     },
