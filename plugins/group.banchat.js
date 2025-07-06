f0f43b08bfd6a6ad60fa8d52fbcd1061aef08a45
YALLICO OWNER <yallico2024@gmail.com>
2025-07-06 12:28:59 +0200

    Eazzy Actualizado V2 🤍
    Eazzy Actualizado V2 🤍

--- plugins/group.banchat.js
+++ plugins/group.banchat.js
@@ -1,10 +1,12 @@
 let handler = async (m, { conn, isAdmin, isROwner }) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    await conn.reply(m.chat, `🧑‍💻BARBOZABOT FUE DESACTIVADO EN ESTE CHAT`, m, rcanal)
+    await conn.reply(m.chat, `🧑‍💻EAZZY FUE BANEADO EN ESTE CHAT POR UM ADMINISTRADOR`, m, rcanal)
     await m.react('✅')
 }
 handler.help = ['banearbot']
 handler.tags = ['group']
 handler.command = ['banearbot', 'banchat']
+handler.group = true 
+handler.admin = true 
 export default handler
